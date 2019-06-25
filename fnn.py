2d8d0dec71df4b2336a1f0fe5f64d7a79daca028
amitadate <email2amitadate@gmail.com>
2019-06-25 16:14:39 -0500

    completed training with logging fnn

--- fnn.py
+++ fnn.py
@@ -1,20 +1,28 @@
 import torch
 from torch import autograd, nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+#from tensorboardX import SummaryWriter
+#writer = SummaryWriter()
+import datetime
 
 if torch.cuda.is_available():
 	print('GPU Available')
+
 else:
 	print('CPU Available')
 
 
 
+
+print(torch.__version__)
 batch_size = 5
 input_size = 5
 hidden_size = 10
 num_classes = 4
 learning_rate = 0.001
-epochs = 1000
+epochs = 10
 
 torch.manual_seed(123)
 
@@ -22,6 +30,37 @@ input = autograd.Variable(torch.rand(batch_size, input_size))
 target = autograd.Variable(torch.rand(batch_size)* num_classes).long()
 
 # print('input', input)
+
+
+def plot_loss(loss,epochs):
+
+
+	idx = []
+	for i in range(epochs):
+		idx.append(i)
+
+
+	# x axis values
+
+	x = idx
+
+	# corresponding y axis values
+	y = epochs
+
+	# plotting the points
+	plt.plot(x, y)
+
+	# naming the x axis
+	plt.xlabel('EPOCHS')
+	# naming the y axis
+	plt.ylabel('LOSS')
+
+	# giving a title to my graph
+	plt.title('Generator Loss f-MNIST')
+	plt.savefig('Generator Loss f-MNIST.png')
+
+	# function to show the plot
+	plt.show()
 
 
 class Net(nn.Module):
@@ -42,24 +81,58 @@ M1 = Net(input_size=input_size, hidden_size=hidden_size, num_classes=num_classes
 opt = torch.optim.Adam(params = M1.parameters(),lr = learning_rate)
 
 
+start = datetime.datetime.now()
+c = [] # list for tracking time
+d = [] # list for tracking loss
+
 for epoch in range(epochs):
-	print('target: ' , target)
+	if d != -1:
+
+		print (" ")
+
+		print(' .. ')
+
+		print('epoch: ',epoch)
+
+		print('target: ' , target)
+		temp_time = datetime.datetime.now()
 
 
 
-	out = M1(input)
-	# print('model', M1)
-	# print('out',out)
+		out = M1(input)
+		# print('model', M1)
+		# print('out',out)
 
-	a,b = out.max(1)
+		a,b = out.max(1)
 
-	print('prediction: ',b)
+		print('prediction: ',b)
 
-	loss = F.nll_loss(out,target)
+		loss = F.nll_loss(out,target)
 
-	print('loss: ',loss.item())
+		print('loss: ',loss.item())
+
+		d.append(loss.item())
 
 
-	M1.zero_grad()
-	loss.backward()
-	opt.step()
+		M1.zero_grad()
+		loss.backward()
+		opt.step()
+
+		print("time for this epoch: ",datetime.datetime.now()-temp_time)
+		print("total time invested in training: ", datetime.datetime.now() -  start)
+
+
+		print (" ")
+
+		print(' .. ')
+
+
+
+
+plot_loss(d,epochs)
+
+
+
+
+
+	# print("current time: ", time.time())
