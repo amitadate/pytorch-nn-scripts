612be9d91b1418da32ee7170b81922cde70a2021
amitadate <email2amitadate@gmail.com>
2019-06-30 02:53:52 -0500

    cnn test train functional

--- cnn.py
+++ cnn.py
@@ -7,6 +7,7 @@ print('**')
 print(' ')
 
 # imports
+import numpy as np
 import torch
 from torch import autograd, nn
 import torch.nn.functional as F
@@ -47,12 +48,12 @@ else:
 print('pytorch version: ',torch.__version__)
 path = os.getcwd()
 input_size = 784
-hidden_size = 500
+hidden_size = 1
 num_classes = 10
-num_epochs = 5
+num_epochs = 1
 batch_size = 100
 learning_rate = 0.001
-time_delay_tqdm = 0.000
+time_delay_tqdm = 0.1
 
 
 # fetching the training and testing datasets from torchvision
@@ -78,7 +79,8 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           shuffle=False)
 
 
-
+print('****************')
+print(len(test_loader.dataset))
 # the nn class defination
 class Net(nn.Module):
 	def __init__(self, input_size, hidden_size, num_classes):
@@ -108,16 +110,23 @@ opt = torch.optim.Adam(params = M1.parameters(),lr = learning_rate)
 
 # ----------- TRAINING BEGINS -------------- #
 
-# training loop
-start = datetime.datetime.now()
+
 
 train_loss_list = [] # list for tracking loss
 
 total_step = len(train_loader)
 
+print(' ')
+print('training started')
+print(' ')
 
+# training loop
+start = datetime.datetime.now()
 
-for epoch in tqdm(range(0,num_epochs),desc = 'training'):
+total = 0
+correct = 0
+
+for epoch in range(0,num_epochs):
 	for i, (images, labels) in enumerate(train_loader):
 
 		temp_time = datetime.datetime.now()
@@ -130,6 +139,9 @@ for epoch in tqdm(range(0,num_epochs),desc = 'training'):
 		outputs = M1(images)
 		loss = criterion(outputs, labels)
 
+		_, predicted = torch.max(outputs.data, 1)
+		# print(predicted)
+
 		# Add loss to list
 		train_loss_list.append(loss.item())
 
@@ -140,132 +152,112 @@ for epoch in tqdm(range(0,num_epochs),desc = 'training'):
 
 		if (i+1) % 100 == 0:
 
-			print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+			#print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+			train_loss_list.append(loss.item())
 
 
 net_time = datetime.datetime.now()-start
-
+print(' ')
+print('training ended')
+print(' ')
 print("total time for training : ",net_time)
+print(' ')
+print('train loss: ',train_loss_list.pop())
+print(' ')
 
 
-plot_loss(train_loss_list,num_epochs) # plotting loss vs epochs
+
+#plot_loss(train_loss_list,num_epochs) # plotting loss vs epochs
 
  # ----------- TRAINING ENDS -------------- #
 
-#  # ----------- TESTING BEGINS -------------- #
-#
-# # testing
-# start = datetime.datetime.now()
-# c = [] # list for tracking time
-# d = [] # list for tracking loss
-#
-# for epoch in tqdm(range(0,epochs),desc = 'training'):
-#
-#
-# # for epoch in range(epochs):
-# 	if d != -1:
-#
-# 		# print (" ")
-# 		#
-# 		# print(' .. ')
-# 		#
-# 		# print('epoch: ',epoch)
-# 		#
-# 		# print('target: ' , target)
-#
-#
-# 		temp_time = datetime.datetime.now()
-#
-#
-# 		out = M1(input) # feed forward through the net
-# 		# print('model', M1)
-# 		# print('out',out)
-#
-# 		a,b = out.max(1) # storing output in b
-#
-# 		# print('prediction: ',b) # displaying the nn output
-#
-# 		loss = F.nll_loss(out,target) # defining the loss
-#
-# 		# print('loss: ',loss.item()) # displaying the loss
-#
-# 		d.append(loss.item()) # storing loss in list for logging purposes
-#
-#
-# 		M1.zero_grad() # making gradients zero
-#
-# 		loss.backward() # backward pass ono autograd variables
-#
-# 		opt.step() # one optmizer step
-# 		#
-# 		# print("time for this epoch: ",datetime.datetime.now()-temp_time)
-# 		#
-# 		# print("total time invested in training: ", datetime.datetime.now() -  start)
-# 		#
-# 		#
-# 		# print ("percentage of training complete: ", ((1-(epoch/epochs)) * 100))
-#
-# 		time.sleep(time_delay_tqdm) #update the tqdm object after the sleeping time
-# net_time = datetime.datetime.now()-start
-#
-# print("total time for training : ",net_time)
-#
-#
-#
-#
-#
-# plot_loss(d,epochs) # plotting loss vs epochs
-#
-#
-#
-# # ----------- TESTING ENDS -------------- #
-#
-#
-# # method to plot graph, input is e - list of values and epochs : number of epochs
-# def plot_loss(e,epochs):
-# 	idx = []
-# 	for i in range(1,epochs+1):
-# 		idx.append(i)
-#
-# 	j = [x * -1 for x in  e]
-#
-# 	y = j
-# 	x = idx
-#
-# 	# plotting the points
-# 	plt.plot(x, y)
-#
-# 	# naming the x axis
-# 	plt.xlabel('EPOCHS')
-# 	# naming the y axis
-# 	plt.ylabel('Train Accuracy')
-#
-# 	# giving a title to my graph
-# 	plt.title('Training ')
-# 	plt.savefig('train.png')
-# 	print('training plot: train.png')
-#
-# 	# function to show the plot
-# 	#plt.show()
-# 	plt.close()
-#
-# 	print("train accuracy: ", y.pop())
-#
-#
-#
-#
-#
-#
-#
-#
-# print(' ')
-# print('**')
-# print('Script End')
-# print('**')
-# print(' ')
-#
-#
-#
-#
-#
-# ## end of script
+ # ----------- TESTING BEGINS -------------- #
+
+# testing loop
+
+test_runs = len(test_loader)
+
+pbar = tqdm(total = len(test_loader))
+
+print(' ')
+print('testing started')
+print(' ')
+
+
+with torch.no_grad():
+	total = 0
+	correct = 0
+	for images,labels in test_loader:
+		images = images.reshape(-1,28*28).to(device)
+		labels = labels.to(device)
+		output = M1(images)
+		_, predicted = torch.max(output.data, 1)
+		total += labels.size(0)
+		correct += (predicted==labels).sum().item()
+		pbar.update(1)
+		time.sleep(time_delay_tqdm)
+
+
+pbar.close()
+
+print(' ')
+print('testing ended')
+print(' ')
+print('Number of test runs: ',total)
+print(' ')
+print('testing accuracy : {} %'.format(100 * correct / total))
+
+
+
+
+# ----------- TESTING ENDS -------------- #
+
+
+# method to plot graph, input is e - list of values and epochs : number of epochs
+def plot_loss(e,epochs):
+	idx = []
+	for i in range(1,epochs+1):
+		idx.append(i)
+
+	j = [x * -1 for x in  e]
+
+	y = j
+	x = idx
+
+	# plotting the points
+	plt.plot(x, y)
+
+	# naming the x axis
+	plt.xlabel('EPOCHS')
+	# naming the y axis
+	plt.ylabel('Train Accuracy')
+
+	# giving a title to my graph
+	plt.title('Training ')
+	plt.savefig('train.png')
+	print('training plot: train.png')
+
+	# function to show the plot
+	#plt.show()
+	plt.close()
+
+	print("train accuracy: ", y.pop())
+
+
+
+
+
+
+
+
+print(' ')
+print('**')
+print('Script End')
+print('**')
+print(' ')
+
+
+
+
+
+## end of script
